810e2e426dcf77611d7b6fb393615123894f1f80
Simply Done <muhammadmominov2004@gmail.com>
2024-11-20 21:22:03 +0500

    super user
    super user

--- Robbi 1/clinic_and_sanatorium/views.py
+++ Robbi 1/clinic_and_sanatorium/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import clinic, clinic_and_sanatorium
+from .serializers import ClinicSerializer, ClinicAndSanatoriumSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ClinicListCreateAPIView(generics.ListCreateAPIView):
+    queryset = clinic.objects.all()
+    serializer_class = ClinicSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ClinicAndSanatoriumListCreateAPIView(generics.ListCreateAPIView):
+    queryset = clinic_and_sanatorium.objects.all()
+    serializer_class = ClinicAndSanatoriumSerializer
--- Robbi 1/config/urls.py
+++ Robbi 1/config/urls.py
@@ -20,13 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('hotels.urls')),
-    path('api/', include('restaurants.urls'))
+    path('api/', include('restaurants.urls')),
+    path('api/', include('clinic_and_sanatorium.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
